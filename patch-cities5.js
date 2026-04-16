eb67709581ea392c1b3e73f34b6f83981c4d172c
Herencia90 <contacto@herencia90.com>
2026-04-16 10:04:13 -0500

    chore: remove temporary patch scripts

--- patch-cities5.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let file = fs.readFileSync('scripts/generate-product-pages.mjs', 'utf8');
-
-file = file.replace(/(\s*match\(product\)\s*\{\s*return includesNormalized\([\s\S]*?\);\s*\})\s*,\s*\{\s*type: 'city',/g, "$1\n  },\n  {\n    type: 'city',");
-
-fs.writeFileSync('scripts/generate-product-pages.mjs', file);
